b9519403c710b219bae0b5884d21be5f917963d7
Jochen Klar <jochenklar@gmail.com>
2016-12-30 22:05:09 +0100

    refactor (config.json, bower, overlays)

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
 var _map;
 
 function init() {
+    $.ajax({
+        url: 'config.json',
+        dataType: 'json',
+        success: function (config) {
+            _map = L.map('map');
 
-    _map = L.map('map');
+            var base = {};
+            $.each(config.base, function(key, layer) {
+                base[layer.name] = L.tileLayer(layer.url, layer.options);
+            });
 
-    var layers = {};
-    $.each(_config.layers, function(key,layer) {
-        layers[layer.name] = L.tileLayer(layer.url, layer.options);
-    });
+            var overlay = {};
+            $.each(config.overlay, function(key,layer) {
+                overlay[layer.name] = L.tileLayer(layer.url, layer.options);
+            }); 
 
-    layers[_config.layers[0].name].addTo(_map);
-    $('.url').text(_config.layers[0].url);
+            base[config.base[0].name].addTo(_map);
 
+            L.control.layers(base, overlay, {
+                collapsed: false
+            }).addTo(_map);
 
-    L.control.layers(layers,{}, {
-        collapsed: false
-    }).addTo(_map);
+            _map.setView(config.locations[0].center, config.locations[0].zoom);
 
-    _map.setView(_config.locations[0].center, _config.locations[0].zoom);
+            $.each(config.locations, function(key,location) {
+                $('.locations').append('<li><a href="#" class="location" data-id="' + key + '">' + location.name + '</a></li>');
+            });
 
-    $.each(_config.locations, function(key,location) {
-        $('.locations').append('<li><a href="#" class="location" data-id="' + key + '">' + location.name + '</a></li>');
-    });
+            $('.location').on('click', function() {
+                var key = parseInt($(this).attr('data-id'), 10);
+                _map.setView(config.locations[key].center, config.locations[key].zoom);
+            });
 
-    $('.location').on('click', function() {
-        var key = parseInt($(this).attr('data-id'), 10);
-        _map.setView(_config.locations[key].center, _config.locations[key].zoom);
-    });
-
-    $('.zoom').text('Zoom level ' + _map.getZoom());
-    _map.on('zoomend', function() {
-        $('.zoom').text('Zoom level ' + _map.getZoom());
+            $('.zoom').text('Zoom level ' + _map.getZoom());
+            _map.on('zoomend', function() {
+                $('.zoom').text('Zoom level ' + _map.getZoom());
+            });
+        },
+        error: function () {
+            console.log('Error with json!');
+        }
     });
 }
 
 $(document).ready(function() {
-    setTimeout('init()',100);
+    setTimeout('init()', 100);
 });
