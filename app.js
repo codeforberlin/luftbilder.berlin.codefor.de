538ee7fd64e12209dac99309ce399c12d73d2eb4
Jochen Klar <jochenklar@gmail.com>
2017-08-05 10:32:16 +0200

    add 2017

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var _map;
 
+var _slider_opt = {
+    start: [ 2018 ],          // most recent year +1
+    direction: 'rtl',         // Put '0' at the bottom of the slider
+    orientation: 'vertical',  // Orient the slider vertically
+    behaviour: 'tap-drag',    // Move handle on tap, bar is draggable
+    step: 150,
+    range: {
+        'min': [1928, 76],    // the second number are the years to the next step
+        '30%': [2004, 3],
+        '45%': [2007, 3],
+        '60%': [2010, 4],
+        '80%': [2014, 2],
+        '90%': [2016, 1],
+        'max': 2017
+    },
+    pips: {
+        mode: 'steps',
+        stepped: true,
+        density: 4
+    }
+};
+
 function init() {
     $.ajax({
         url: 'config.json',
@@ -22,26 +44,7 @@ function init() {
 
             var slider = document.getElementById('slider');
 
-            noUiSlider.create(slider, {
-                start: [ 2016 ],
-                direction: 'rtl', // Put '0' at the bottom of the slider
-                orientation: 'vertical', // Orient the slider vertically
-                behaviour: 'tap-drag', // Move handle on tap, bar is draggable
-                step: 150,
-                range: {
-                    'min': [1928, 76],
-                    '50%': [2004, 3],
-                    '62.5%': [2007, 3],
-                    '75%': [2010, 4],
-                    '90%': [2014, 2],
-                    'max': 2016
-                },
-                pips: {
-                    mode: 'steps',
-                    stepped: true,
-                    density: 4
-                }
-            });
+            noUiSlider.create(slider, _slider_opt);
 
             slider.noUiSlider.on('set', function() {
                 var new_layer_name = Math.floor(this.get())
